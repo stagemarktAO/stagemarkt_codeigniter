fdc67e2123c8aeb52c2ea2039e7aeb7cf3f672cf
jmuilwijk <jamy0184@gmail.com>
2017-10-06 09:51:16 +0200

    Tool tip fixed with javascript

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="<?=base_url()?>assets/js/main.js"></script>
 	</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
 	<head>
 		<title>Stagemarkt</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href=<?=base_url()?>"assets/css/AdminLTE.min.css">
-		<link rel="stylesheet" href=<?=base_url()?>"assets/css/main.css">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/AdminLTE.min.css">
+		<link rel="stylesheet" href="<?=base_url()?>assets/css/main.css">
 	</head>
 	<body>
