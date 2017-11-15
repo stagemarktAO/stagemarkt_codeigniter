eb1fa9009a32468a45cceae9c1ab4fd002c217bd
jmuilwijk <jamy0184@gmail.com>
2017-11-15 11:27:53 +0100

    adminLTE loaded through CDN

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,7 @@
 	<head>
 		<title>Stagemarkt</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?=base_url();?>assets/css/AdminLTE.min.css">
+        <link rel="stylesheet" https="://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.2/css/AdminLTE.min.css">
 		<link rel="stylesheet" href="<?=base_url();?>assets/css/main.css">
         <link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap-datepicker.min.css">
 	</head>
