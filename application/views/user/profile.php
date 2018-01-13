6c7699f206d7822b0ede09b8d59a2c05aee3e08a
mattiassuijker <poldosie@gmail.com>
2018-01-13 14:52:32 +0100

    profile + changes

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -6,35 +6,36 @@
  * Time: 10:57
  */
     echo validation_errors();
-    echo form_open('user/profile', 'class="form-horizontal col-sm-10"');
+    echo form_open('user/profile', 'class="form-horizontal col-sm-10" onSubmit="alert(\'Als u iets in uw profiel veranderd heeft is dit gelukt.\');"');
 ?>
-<div class="box-body">
-    <div class="form-group">
-        <label for="fname" class="col-sm-2 control-label">name:</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" name="fname" value="<?php echo $_SESSION['fname'];?>">
+    <div class="box-body">
+        <div class="form-group">
+            <label for="fname" class="col-sm-2 control-label">naam:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="fname" value="<?php echo $_SESSION['fname'];?>">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="lname" class="col-sm-2 control-label">achter naam:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="lname" value="<?php echo $_SESSION['lname'];?>">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="phone" class="col-sm-2 control-label">telefoon nummer:</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" value="<?php echo $_SESSION['phone'];?>" name="phone" data-toggle="tooltip" data-placement="bottom" title="please enter a valid phone number (without special characters)">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="email" class="col-sm-2 control-label">email:</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" name="email" value="<?php echo $_SESSION['email'];?>">
+            </div>
         </div>
     </div>
-    <div class="form-group">
-        <label for="lname" class="col-sm-2 control-label">Last name:</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" name="lname" value="<?php echo $_SESSION['lname'];?>">
-        </div>
+    <div class="box-footer">
+        <button class="btn btn-info pull-right" type="submit" name="submit" value="Update">Verzend</button>
+        <a class="btn btn-default" href="<?= base_url('') ?>">Annuleren</a>
     </div>
-    <div class="form-group">
-        <label for="phone" class="col-sm-2 control-label">Phonenumber:</label>
-        <div class="col-sm-10">
-            <input type="number" class="form-control" value="<?php echo $_SESSION['phone'];?>" name="phone" data-toggle="tooltip" data-placement="bottom" title="please enter a valid phone number (without special characters)">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="email" class="col-sm-2 control-label">email:</label>
-        <div class="col-sm-10">
-            <input type="email" class="form-control" name="email" value="<?php echo $_SESSION['email'];?>">
-        </div>
-    </div>
-</div>
-<div class="box-footer">
-    <button class="btn btn-info pull-right" type="submit" name="submit" value="Update">Verzend</button>
-</div>
 </form>
