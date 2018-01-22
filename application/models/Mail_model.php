2dc91d79081a7f3ae57df0085e64c4134fe534e6
jmuilwijk <jamy0184@gmail.com>
2018-01-22 15:33:21 +0100

    update

--- application/models/Mail_model.php
+++ application/models/Mail_model.php
@@ -13,7 +13,8 @@ class Mail_model extends CI_Model
         parent::__construct();
     }
 
-    public function send_welcome_mail()
+    // private function template so just we can use it
+    private function email_template($subject, $body, $receiver)
     {
         $this->load->library('PHPMailer/Phpmailer');
         $mail = New CI_PHPMailer();
@@ -34,23 +35,43 @@ class Mail_model extends CI_Model
         $mail->FromName    = 'Project stagemarkt';
         $mail->WordWrap    = 900;
 
-        $mail->addAddress($this->input->post('email'));      // Add a recipient
+        $mail->addAddress($receiver);                        // Add a recipient
 
-        $mail->isHTML(true);                           // Set email format to HTML
+        $mail->isHTML(TRUE);                           // Set email format to HTML
         $mail->ContentType = 'text/html; charset=utf-8\r\n';
-        $mail->Subject = 'Welcome!';                          // Subject
-        $mail->Body    = 'Je bent succesvol geregistreerd.';  // Message
+        $mail->Subject = $subject;                          // Subject
+        $mail->Body    = $body;                             // Message
 
         if(!$mail->send()) {
-            return false;
+            return FALSE;
         } else {
-            return true;
+            return TRUE;
+        }
+    }
+
+    public function send_welcome_mail()
+    {
+        if($this->email_template('Welcome!', 'Je bent succesvol geregistreerd.', $this->input->post('email'))) {
+            return TRUE;
+        } else {
+            return FALSE;
         }
     }
 
     public function send_recovery_email()
     {
-        // send email
-        // use the above function' template in a new function
+        /**
+         * recovery link, link to a User.php function to give the user a form with the option
+         * to give up a new password to then login with.
+         *
+         * the given password must be hashed with the same algorithm as in the register function.
+         */
+        $body = 'recovery link';
+        // send email from the template function
+        if($this->email_template('Recovery', $body, $this->input->post('email'))) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 }
