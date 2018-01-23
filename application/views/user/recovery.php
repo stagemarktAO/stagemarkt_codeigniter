79faf742c4f1260cbdc98d28d6f2372fff556901
jmuilwijk <jamy0184@gmail.com>
2018-01-23 11:55:40 +0100

    quick commit
    gonna test roans code

--- application/views/user/recovery.php
+++ application/views/user/recovery.php
@@ -4,18 +4,28 @@
     <title>Stagemarkt</title>
 </head>
 <body class="hold-transition login-page"></body>
-<div>
-    <!-- form send to Mail_controller to send an email-->
-    <?php form_open('Mail_controller/reset_password'); ?>
-        <div class="form-group has-feedback">
-            <input type="email" name="email" placeholder="email" class="form-control" value="<?php echo set_value('email'); ?>" /><br />
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+<div class="login-box">
+    <div class="login-logo">
+        <a href="<?= base_url('') ?>">
+            <b>STAGE</b>MARKT
+        </a>
+    </div>
+    <div class="login-box-body">
+        <?php echo validation_errors(); ?>
+        <div>
+            <!-- form send to Mail_controller to send an email-->
+            <?php form_open('Mail_controller/reset_password'); ?>
+                <!--submit emailadress for recover email-->
+                <div class="form-group has-feedback">
+                    <input type="email" name="email" placeholder="email" class="form-control" value="<?php echo set_value('email'); ?>" /><br />
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                </div>
+                <div class="col-xs-4">
+                    <button type="submit" name="submit" value="submit" class="btn btn-primary btn-block btn-flat">Submit</button>
+                </div>
+            </form>
         </div>
-        <div class="col-xs-4">
-            <button type="submit" value="submit" class="btn btn-primary btn-block btn-flat">Submit</button>
-        </div>
-    </form>
+    </div>
 </div>
 </body>
 </html>
-<!--submit username/emailadress for recover email-->
